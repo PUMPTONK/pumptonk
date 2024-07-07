6ad6649d79daf6216e7ad2d8aad56ea922c4c4c5
czarex <51121780+czarex@users.noreply.github.com>
2024-07-07 13:17:02 -0700

    Create walletRoutes.js
    Wallet routes to connect to TON and retrieve wallet details

---
+++ routes/walletRoutes.js
@@ -0,0 +1,10 @@
+// routes/walletRoutes.js
+
+const express = require('express');
+const { connectWallet } = require('../controllers/walletController');
+
+const router = express.Router();
+
+router.post('/connect', connectWallet);
+
+module.exports = router;
