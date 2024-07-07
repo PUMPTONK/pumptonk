214b42821745896488c6a9d848fbe71a4ac1a56b
czarex <51121780+czarex@users.noreply.github.com>
2024-07-07 16:38:28 -0700

    Update userRoutes.js
    Updated route to retrieve user details

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 const upload = require('../middleware/uploadMiddleware');
 
 router.post('/update', upload.single('profilePicture'), userController.updateUser);
+router.get('/:walletAddress', userController.getUserDetails);
 
 module.exports = router;
