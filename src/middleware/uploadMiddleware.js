21de497f72d957198d88afa780ea494ea8c6f95f
czarex <51121780+czarex@users.noreply.github.com>
2024-07-07 15:36:11 -0700

    Create uploadMiddleware.js
    Profile picture Upload Middle WARE

---
+++ src/middleware/uploadMiddleware.js
@@ -0,0 +1,25 @@
+// src/middleware/uploadMiddleware.js
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/profile_pictures/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ['image/jpeg', 'image/png'];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type. Only JPEG and PNG are allowed.'));
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+module.exports = upload;
