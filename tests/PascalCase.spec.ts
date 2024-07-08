2742596e6a664c88699c94299fc0014299ae392f
czarex <51121780+czarex@users.noreply.github.com>
2024-07-08 10:59:35 -0700

    Create PascalCase.spec.ts
    Create PascalCase.spec.ts

---
+++ tests/PascalCase.spec.ts
@@ -0,0 +1,81 @@
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Cell, toNano } from '@ton/core';
+import { PascalCase } from '../wrappers/PascalCase';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+
+describe('PascalCase', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('PascalCase');
+    });
+
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let pascalCase: SandboxContract<PascalCase>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        pascalCase = blockchain.openContract(
+            PascalCase.createFromConfig(
+                {
+                    id: 0,
+                    counter: 0,
+                },
+                code
+            )
+        );
+
+        deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await pascalCase.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: pascalCase.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and pascalCase are ready to use
+    });
+
+    it('should increase counter', async () => {
+        const increaseTimes = 3;
+        for (let i = 0; i < increaseTimes; i++) {
+            console.log(`increase ${i + 1}/${increaseTimes}`);
+
+            const increaser = await blockchain.treasury('increaser' + i);
+
+            const counterBefore = await pascalCase.getCounter();
+
+            console.log('counter before increasing', counterBefore);
+
+            const increaseBy = Math.floor(Math.random() * 100);
+
+            console.log('increasing by', increaseBy);
+
+            const increaseResult = await pascalCase.sendIncrease(increaser.getSender(), {
+                increaseBy,
+                value: toNano('0.05'),
+            });
+
+            expect(increaseResult.transactions).toHaveTransaction({
+                from: increaser.address,
+                to: pascalCase.address,
+                success: true,
+            });
+
+            const counterAfter = await pascalCase.getCounter();
+
+            console.log('counter after increasing', counterAfter);
+
+            expect(counterAfter).toBe(counterBefore + increaseBy);
+        }
+    });
+});
