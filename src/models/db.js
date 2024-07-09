aeb3cf8064ef94baa6e88677278961d0692d0483
czarex <51121780+czarex@users.noreply.github.com>
2024-07-09 07:08:52 -0700

    Create db.js
    Create db.js

---
+++ src/models/db.js
@@ -0,0 +1,14 @@
+// src/models/db.js
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'yciyc-tokens',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool.promise();
