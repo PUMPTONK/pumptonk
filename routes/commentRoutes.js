68cdefcf756f51da5b5d03c774897d53cd005f66
czarex <51121780+czarex@users.noreply.github.com>
2024-07-08 18:26:34 -0700

    Update commentRoutes.js
    Minor fix

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -7,6 +7,8 @@ const {
   getCommentByTokenAndUser,
   getAllComments,
   searchCommentsByUser,
+  getTotalCommentsOnToken,
+  getTotalCommentsOnAllTokens,
 } = require('../controllers/commentsController');
 
 const router = express.Router();
@@ -17,5 +19,7 @@ router.delete('/comments/:id', deleteComment);
 router.get('/comments/:tokenId/:userId', getCommentByTokenAndUser);
 router.get('/comments', getAllComments);
 router.get('/comments/:userId', searchCommentsByUser);
+router.get('/comments/total/token/:tokenId', getTotalCommentsOnToken); // New route for total comments on a token
+router.get('/comments/total/all', getTotalCommentsOnAllTokens); // New route for total comments on all tokens
 
 module.exports = router;
