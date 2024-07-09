2d0bd2c8cedc1dde5d1a40540f3babc4562578d8
czarex <51121780+czarex@users.noreply.github.com>
2024-07-09 07:10:17 -0700

    Create followRoutes.js
    Create followRoutes.js

---
+++ src/routes/followRoutes.js
@@ -0,0 +1,12 @@
+// src/routes/followRoutes.js
+const express = require('express');
+const followController = require('../controllers/followController');
+
+const router = express.Router();
+
+router.post('/follow', followController.follow);
+router.post('/unfollow', followController.unfollow);
+router.get('/followers/:user_id', followController.getFollowers);
+router.get('/following/:user_id', followController.getFollowing);
+
+module.exports = router;
