c255149ef67c461980df10ef9d99e72ab6e52deb
czarex <51121780+czarex@users.noreply.github.com>
2024-07-09 19:05:05 -0700

    Update user.js
    Updated table columns

--- models/user.js
+++ models/user.js
@@ -3,9 +3,13 @@ const pool = require('../config/dbConfig');
 const createUserTable = async () => {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS users (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      wallet_address VARCHAR(255) NOT NULL UNIQUE,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        id int(11) NOT NULL,
+        walletAddress varchar(255) NOT NULL,
+        name varchar(255) NOT NULL,
+        description text DEFAULT NULL,
+        profilePicture varchar(255) DEFAULT NULL,
+        created_at timestamp NOT NULL DEFAULT current_timestamp(),
+        updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
     )
   `);
 };
