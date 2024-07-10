cc5717e7f101a5e4dcfe4d5b7aa7c0233378753c
czarex <51121780+czarex@users.noreply.github.com>
2024-07-09 19:01:49 -0700

    Update token.js
    Updated API endpoints to return the following:
    1.  Bonding Curve Price 
    2.  Token Transaction Hash
    3.  Initial supply and Current supply
    4.  Market Cap

--- models/token.js
+++ models/token.js
@@ -3,17 +3,25 @@ const pool = require('../config/dbConfig');
 const createTokenTable = async () => {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS tokens (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      symbol VARCHAR(255) NOT NULL,
-      description TEXT,
-      token_image TEXT,
-      initial_supply DECIMAL(18, 8) NOT NULL,
-      twitter_link TEXT,
-      telegram_link TEXT,
-      website_link TEXT,
-      user_wallet_address VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      id int(11) NOT NULL,
+      user_id float NOT NULL,
+      name varchar(255) NOT NULL,
+      symbol varchar(255) NOT NULL,
+      description text DEFAULT NULL,
+      token_image text DEFAULT NULL,
+      initial_supply decimal(18,8) NOT NULL,
+      twitter_link text DEFAULT NULL,
+      telegram_link text DEFAULT NULL,
+      website_link text DEFAULT NULL,
+      user_wallet_address varchar(255) NOT NULL,
+      allocated_to_user_wallet varchar(255) NOT NULL,
+      allocated_to_market_maker_wallet varchar(255) NOT NULL,
+      allocated_to_exchange_wallets varchar(255) NOT NULL,
+      created_at timestamp NOT NULL DEFAULT current_timestamp(),
+      marketCap decimal(18,2) DEFAULT NULL,
+      currentSupply int(11) DEFAULT NULL,
+      transaction_hash varchar(255) DEFAULT NULL,
+      bondingCurvePrice float NOT NULL
     )
   `);
 };
