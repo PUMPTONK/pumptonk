dbd5189348bbf93876009204986090e3afebb1b4
czarex <51121780+czarex@users.noreply.github.com>
2024-07-11 03:24:19 -0700

    Create db.js
    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+// db.js
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'yciyc-tokens'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+module.exports = connection;
